ea7d4a019f20e56d735b387865ba1f9ca42644fa
Maria Nunez <mariaalician@gmail.com>
2018-06-09 17:41:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,18 +11,4 @@ class Dog
     @name
   end
   
-    def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
-  
 end
-
-lassie = Dog.new("Lassie")
